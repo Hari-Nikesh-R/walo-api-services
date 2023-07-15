c42be1ce55ca1427e5c44ee7fd3d104abe19e4f4
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2023-07-15 16:40:51 +0530

    initial commmit

---
+++ src/main/java/com/sns/waloapiservices/WaloApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.sns.waloapiservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaloApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaloApiServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sns/waloapiservices/WaloApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns.waloapiservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaloApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

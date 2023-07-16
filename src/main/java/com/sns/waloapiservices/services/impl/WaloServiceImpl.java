452b8ea3c94320c9166e85832dc7104c165f98c5
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2023-07-16 09:51:17 +0530

    commit

--- src/main/java/com/sns/waloapiservices/services/impl/WaloServiceImpl.java
+++ src/main/java/com/sns/waloapiservices/services/impl/WaloServiceImpl.java
@@ -2,18 +2,14 @@ package com.sns.waloapiservices.services.impl;
 
 import com.fasterxml.jackson.databind.ser.Serializers;
 import com.sns.waloapiservices.dtos.BaseResponse;
-import com.sns.waloapiservices.model.Accidents;
-import com.sns.waloapiservices.model.AppliedJobs;
-import com.sns.waloapiservices.model.BankDetails;
-import com.sns.waloapiservices.model.PostArticles;
-import com.sns.waloapiservices.repository.AccidentsRepository;
-import com.sns.waloapiservices.repository.AppliedJobsRepository;
-import com.sns.waloapiservices.repository.BankDetailsRepository;
-import com.sns.waloapiservices.repository.PostArticleRepository;
+import com.sns.waloapiservices.model.*;
+import com.sns.waloapiservices.repository.*;
 import com.sns.waloapiservices.services.WaloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.security.PublicKey;
 
 @Service
 public class WaloServiceImpl implements WaloService {
@@ -25,7 +21,7 @@ public class WaloServiceImpl implements WaloService {
     public BaseResponse<Object> postArticle(PostArticles postArticles) {
         try {
             PostArticles optionalPostArticles = postArticleRepository.save(postArticles);
-            return BaseResponse.builder().statusCode(HttpStatus.FORBIDDEN.value()).data(optionalPostArticles).errorDesc(null).result("Posted article").build();
+            return BaseResponse.builder().statusCode(HttpStatus.OK.value()).data(optionalPostArticles).errorDesc(null).result("Posted article").build();
         }
         catch (Exception exception) {
             return BaseResponse.builder().statusCode(HttpStatus.FORBIDDEN.value()).data(null).errorDesc(exception.getLocalizedMessage()).result(exception.getMessage()).build();
@@ -39,7 +35,7 @@ public class WaloServiceImpl implements WaloService {
     public  BaseResponse<Object> postAccidents(Accidents accidents) {
         try {
             Accidents optionalAccidents = accidentsRepository.save(accidents);
-            return BaseResponse.builder().statusCode(HttpStatus.FORBIDDEN.value()).data(optionalAccidents).errorDesc(null).result("Reported Accidents").build();
+            return BaseResponse.builder().statusCode(HttpStatus.OK.value()).data(optionalAccidents).errorDesc(null).result("Reported Accidents").build();
         }
         catch (Exception exception){
             return BaseResponse.builder().statusCode(HttpStatus.FORBIDDEN.value()).data(null).errorDesc(exception.getLocalizedMessage()).result(exception.getMessage()).build();
@@ -53,7 +49,7 @@ public class WaloServiceImpl implements WaloService {
     public BaseResponse<Object> postAppliedJobs(AppliedJobs appliedJobs){
         try{
             AppliedJobs optionalAppliedJobs =  appliedJobsRepository.save(appliedJobs);
-            return BaseResponse.builder().statusCode(HttpStatus.FORBIDDEN.value()).data(optionalAppliedJobs).errorDesc(null).result("Apply for Jobs").build();
+            return BaseResponse.builder().statusCode(HttpStatus.OK.value()).data(optionalAppliedJobs).errorDesc(null).result("Apply for Jobs").build();
         }
         catch (Exception exception){
             return BaseResponse.builder().statusCode(HttpStatus.FORBIDDEN.value()).data(null).errorDesc(exception.getLocalizedMessage()).result(exception.getMessage()).build();
@@ -67,7 +63,21 @@ public class WaloServiceImpl implements WaloService {
     public BaseResponse<Object> postBankDetails (BankDetails bankDetails){
         try{
             BankDetails optionalBankDetails =  bankDetailsRepository.save(bankDetails);
-            return BaseResponse.builder().statusCode(HttpStatus.FORBIDDEN.value()).data(optionalBankDetails).errorDesc(null).result("Bank Details").build();
+            return BaseResponse.builder().statusCode(HttpStatus.OK.value()).data(optionalBankDetails).errorDesc(null).result("Bank Details").build();
+        }
+        catch (Exception exception){
+            return BaseResponse.builder().statusCode(HttpStatus.FORBIDDEN.value()).data(null).errorDesc(exception.getLocalizedMessage()).result(exception.getMessage()).build();
+        }
+    }
+
+    @Autowired
+    private LegalDiscussionRepository legalDiscussionRepository;
+
+    @Override
+    public BaseResponse<Object> postLegalDiscussion (LegalDiscussion legalDiscussion){
+        try{
+            LegalDiscussion optionalLegalDiscussion = legalDiscussionRepository.save(legalDiscussion);
+            return BaseResponse.builder().statusCode(HttpStatus.OK.value()).data(optionalLegalDiscussion).errorDesc(null).result("Legal Discussion").build();
         }
         catch (Exception exception){
             return BaseResponse.builder().statusCode(HttpStatus.FORBIDDEN.value()).data(null).errorDesc(exception.getLocalizedMessage()).result(exception.getMessage()).build();
